28c762c4a0a91e5238e6f01a77abf55c2c2ba093
jozef rudy <jozef.rudy@gmail.com>
2025-11-26 10:23:20 +0100

    add google client and test it

--- src/WebGrader.Core/Api/GoogleClient.fs
+++ src/WebGrader.Core/Api/GoogleClient.fs
@@ -13,25 +13,37 @@ type SearchResult = {
     snippet: string
 }
 
+type SearchInformation = {
+    totalResults: string
+    searchTime: float option
+}
+
 type SearchResponse = {
     items: SearchResult list option
+    searchInformation: SearchInformation option
 }
 
 type GoogleClient(client: HttpClient, config: GoogleConfig) =
-    
-    member this.Search (query: string) (ct: CancellationToken) =
+
+    member this.SearchFull (query: string) (ct: CancellationToken) =
         taskResult {
             try
-                let encodedQuery = Uri.EscapeDataString(query)
+                let encodedQuery = Uri.EscapeDataString query
                 let url = $"?key={config.ApiKey}&cx={config.SearchEngineId}&q={encodedQuery}"
-                
+
                 let! response = client.GetAsync(url, ct)
-                
+
                 if not response.IsSuccessStatusCode then
                     return! Error(exn $"Google API error {response.StatusCode}")
                 else
-                    let! result = response.Content.ReadFromJsonAsync<SearchResponse>(ct)
-                    return result.items |> Option.defaultValue []
+                    let! result = response.Content.ReadFromJsonAsync<SearchResponse> ct
+                    return result
             with ex ->
                 return! Error ex
+        }
+
+    member this.Search (query: string) (ct: CancellationToken) =
+        taskResult {
+            let! response = this.SearchFull query ct
+            return response.items |> Option.defaultValue []
         }
